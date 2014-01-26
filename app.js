5bd590662d941b7cbfff035fe22839cd44e24b22
Ezeki Zibzibadze <playerx@users.noreply.github.com>
2014-01-26 10:40:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,60 @@
 
+var radio = require('radio-stream');
+var $ = require('jQuery').create();
 
-var radio = require('./radio-stream')
-var http = require('http')
+var API_ROOT_URL = 'http://api.jok.io';
 
-var channelsCount = 0;
 
-http.get('http://api.jok.ge/musicchannel/0/getall', function (res) {
-
-    var st = '';
-
-    res.on('data', function (data) {
-        st += data;
-    });
-
-    res.on('end', function () {
-        var channels = JSON.parse(st);
-
-        console.log('Channels loaded: ', channels.length);
-        channelsCount = channels.length;
-        
-        for (var i = 0; i < channels.length; i++) {
-            CreateStream(channels[i].ID, channels[i].Source);
-        }
-    });
-});
-
-function CreateStream(id, url) {
+function startReadingMetadata(id, url, nextTimeoutDelayCount) {
     var stream = radio.createReadStream(url);
 
     stream.on("metadata", function (metadata) {
 
+        nextTimeoutDelayCount = 1;
+
         var title = radio.parseMetadata(metadata).StreamTitle;
         if (!title || title == '') return;
 
-        http.get('http://api.jok.ge/musicchannel/' + id + '/addtrack?info=' + title);
+        console.log(metadata);
+
+        $.get(API_ROOT_URL + '/Music/BroadcastTrack/?channelID=' + id + '&trackInfo=' + title, function () {
+            console.log(id, title, 'success');
+        });
     });
 
-    stream.on("error", function (error) {
-        console.log(id, error);
+    stream.on("close", function (info) {
+        console.log(id, 'close', info, nextTimeoutDelayCount);
+        reconnect();
     });
+
+    stream.on("error", function (info) {
+        console.log(id, 'error', info, nextTimeoutDelayCount);
+        reconnect();
+    });
+
+
+    function reconnect() {
+        setTimeout(function () {
+
+            if (nextTimeoutDelayCount < 30)
+                nextTimeoutDelayCount++;
+
+            startReadingMetadata(id, url, nextTimeoutDelayCount);
+
+        }, nextTimeoutDelayCount * 1000);
+    }
 }
+
+
+$.get(API_ROOT_URL + '/Music/Channels', function (res) {
+
+    if (!res || !res.IsSuccess) return;
+
+    for (var i = 0; i < res.Data.length; i++) {
+
+        startReadingMetadata(res.Data[i].ID, res.Data[i].Source, 1);
+    }
+})
+
+console.log('Started! :)');
+
